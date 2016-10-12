25220cebca4a4c169a5e0167bebd27c40cf33638
aik <aik>
2016-10-12 18:50:49 +0100

    update server code

---
+++ index.js
@@ -0,0 +1,25 @@
+var Express = require('express');
+var app = Express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+app.get('/', function(req, res, next){
+    // has to redirect this to the client first page
+    res.send('Hello From Server');
+});
+
+var options = {
+    debug: true
+}
+
+var server = require('http').createServer(app);
+app.use('/peerjs', ExpressPeerServer(server, options));
+
+server.listen(9000);
+
+server.on('connection', function(id){
+    console.log('someone connected' + id);
+});
+
+server.on('disconnect', function(id){
+    console.log('someone disconnected' + id);
+});
