1a5d3edc03400da7b2b8f880fc3411b0edbb7703
aik6980 <aik6980>
2016-10-14 09:01:58 +0100

    update game code

--- public/js/game.js
+++ public/js/game.js
@@ -18,3 +18,36 @@ var peer = new Peer({
 peer.on('open', function(id){
 	document.getElementById("peer_info").innerHTML = 'my id: ' + id;
 });
+
+
+// resources
+var game;
+
+// phaser game
+// http://www.lessmilk.com/tutorial/2d-platformer-phaser
+var main_state = {
+	
+	preload: function(){
+
+	},
+
+	create: function (){
+		// create a bitmap data
+		// http://phaser.io/examples/v2/bitmapdata/cached-bitmapdata
+		var bmd = game.add.bitmapData(32, 32);
+		bmd.context.fillStyle =  'rgb(255,0,0)';
+		bmd.context.fillRect(0,0,32,32);
+
+		game.cache.addBitmapData('red32', bmd);
+
+		game.add.sprite(10,10,game.cache.getBitmapData('red32'));
+	},
+
+	update: function(){
+
+	}
+};
+
+game = new Phaser.Game(256, 256);
+game.state.add('main', main_state);
+game.state.start('main');
